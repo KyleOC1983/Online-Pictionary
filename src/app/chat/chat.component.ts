ac0640d8904c45dca0e3fb11dac45d48f3629536
Your Name <you@example.com>
2020-08-11 14:06:27 -0500

    adds snackbars, filler correct answer

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { Message } from '../interfaces/message.interface'
 import { ChatService } from '../services/chat.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-chat',
@@ -10,28 +11,45 @@ import { ChatService } from '../services/chat.service';
 export class ChatComponent implements OnInit {
   @ViewChild('output') private chatOutput: ElementRef;
   messages: Array<Message> = [];
-  messageText: string = ''
-  displayName: string = ''
+  messageText: string = '';
+  displayName: string = '';
+  answer = "generic answer";
 
-  constructor(private chatService: ChatService) { }
+  constructor(private chatService: ChatService, private _snackBar: MatSnackBar) { }
 
 
-  sendMessage(){
-    if(this.messageText.length > 0 && this.messageText.length <= 280){
+  sendMessage() {
+    if (this.messageText.length > 0 && this.messageText.length <= 280 && this.messageText == this.answer) {
       let msg: Message = {
         username: this.displayName,
-        body: this.messageText,
+        body: this.messageText
       }
       this.chatService.sendChat(msg);
       this.messageText = '';
+      this._snackBar.open('That is correct!', 'OK', {
+        duration: 2000,
+      });
+    }
+    else if(this.messageText.length > 0 && this.messageText.length <= 280 && this.messageText != this.answer) {
+      let msg: Message = {
+        username: this.displayName,
+        body: this.messageText
+      }
+      this.chatService.sendChat(msg);
+      this.messageText = '';
+      this._snackBar.open('That is incorrect :(', 'OK', {
+        duration: 2000,
+      });
     }
   }
 
   chatScroll(): void {
     try {
-        this.chatOutput.nativeElement.scrollTo(0, this.messages.length * 20);
-    } catch(err) { }                 
-}
+      this.chatOutput.nativeElement.scrollTo(0, this.messages.length * 20);
+    } catch (err) { }
+  }
+
+
 
 
   ngOnInit(): void {
