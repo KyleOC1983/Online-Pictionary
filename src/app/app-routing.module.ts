dfb0ec3ed3046f9b13bbded636e2c50a06433fe0
Connor Kennedy <connorkennedy@Connors-MacBook-Air.local>
2020-08-11 13:14:19 -0500

    adds routes, moved sketchpad to game component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { GameComponent } from './game/game.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: GameComponent},
+  {path: '**', component: GameComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
