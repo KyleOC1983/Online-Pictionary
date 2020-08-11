ada7b4279b8f731bbb9d83a5915fb6e8669936e6
Your Name <you@example.com>
2020-08-11 11:16:45 -0500

    adds message sending and chat scroll functionality

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+import { Message } from '../interfaces/message.interface'
+import { ChatService } from '../services/chat.service';
 
 @Component({
   selector: 'app-chat',
@@ -6,8 +8,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chat.component.scss']
 })
 export class ChatComponent implements OnInit {
+  @ViewChild('output') private chatOutput: ElementRef;
+  messages: Array<Message> = [];
+  messageText: string = ''
+  displayName: string = ''
 
-  constructor() { }
+  constructor(private chatService: ChatService) { }
+
+
+  sendMessage(){
+    if(this.messageText.length > 0 && this.messageText.length <= 280){
+      let msg: Message = {
+        username: this.displayName,
+        body: this.messageText,
+      }
+      this.chatService.sendChat(msg);
+      this.messageText = '';
+    }
+  }
+
+  chatScroll(): void {
+    try {
+        this.chatOutput.nativeElement.scrollTo(0, this.messages.length * 20);
+    } catch(err) { }                 
+}
+
 
   ngOnInit(): void {
   }
