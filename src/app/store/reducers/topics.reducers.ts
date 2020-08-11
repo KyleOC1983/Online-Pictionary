23db195c95bd43cf9495dbc2d4b99e6e9ac8099a
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2020-08-11 14:44:01 -0500

    completed topic state managment

---
+++ src/app/store/reducers/topics.reducers.ts
@@ -0,0 +1,31 @@
+import { createReducer, on, Action } from '@ngrx/store';
+import { stat } from 'fs';
+import * as topicsActions from '../actions/topics.actions'
+
+export interface TopicState {
+    currentTopic: string,
+    usedTopics: string[],
+    topics: string[],
+};
+
+export const initalTopicState: TopicState = {
+    currentTopic: null,
+    usedTopics: [],
+    topics: ["Apple", "Chair", "Mouse", "Moon", "Ant", "Computer", "Book", "Pig", "Eraser", "Vase"]
+};
+
+const reducer = createReducer(initalTopicState,
+    on(topicsActions.selectTopic, (state) =>{
+        let topics = [...state.topics]
+        let used = [...state.usedTopics]
+        if(state.currentTopic){
+            used = [...used, state.currentTopic]
+        }
+        if(topics.length === 0){
+            topics = used
+            used = []
+        }
+        const random = Math.floor(Math.random() * topics.length);
+        let newTopic = topics.splice(random, 1)[0];
+        return{...state, currentTopic: newTopic, usedTopics: used, topics: topics}
+        }));
