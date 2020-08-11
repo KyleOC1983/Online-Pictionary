379d51c33f07c59d9d2817dcf6d011137c0238c7
Connor Kennedy <connorkennedy@Connors-MacBook-Air.local>
2020-08-11 15:02:16 -0500

    basic starting framework for backend sockets

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 3000;
+const io = require('./server/config/socket.conf').listen(app);
 
 app.use(bodyParser.json());
 app.use(express.static(__dirname+"/dist"));
 app.get('*', (req, res)=>{
     res.sendFile('/dist/index.html', { root: __dirname + '/'});
 });
+app.use(io);
 
 app.listen(port);
