79850f687228d549973e83ace9578d100cb092b4
Mike Truax <mike@miketruax.com>
2020-08-11 09:36:25 -0500

    iniital scaffolding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { HomeComponent } from './home/home.component';
+import { ChatComponent } from './chat/chat.component';
+import { SketchpadComponent } from './sketchpad/sketchpad.component';
+import { GameComponent } from './game/game.component';
+import { StoreModule } from '@ngrx/store';
+import { MaterialModule } from './materialModule';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
+    HomeComponent,
+    ChatComponent,
+    SketchpadComponent,
+    GameComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    StoreModule.forRoot({}, {})
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/sketchpad/sketchpad.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SketchpadComponent } from './sketchpad.component';
+
+describe('SketchpadComponent', () => {
+  let component: SketchpadComponent;
+  let fixture: ComponentFixture<SketchpadComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ SketchpadComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SketchpadComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
