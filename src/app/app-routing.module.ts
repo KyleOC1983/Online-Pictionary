4344e46fe8e99e2aedba5affab278945709d7d16
Connor Kennedy <connorkennedy@Connors-MacBook-Air.local>
2020-08-11 13:19:05 -0500

    updated routes to add some future routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { GameComponent } from './game/game.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
 
 const routes: Routes = [
-  {path: '', component: GameComponent},
-  {path: '**', component: GameComponent}
+  {path: 'game', component: GameComponent},
+  {path: '', redirectTo: '/game', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'signup', component: SignupComponent},
+  {path: '**', redirectTo: '/game'}
 ];
 
 @NgModule({
