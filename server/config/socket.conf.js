686e98dfa028cfd7c4e6a4dad070ca7476705f89
Connor Kennedy <connorkennedy@Connors-MacBook-Air.local>
2020-08-12 10:41:28 -0500

    initial backed setup for room-joining capabilities

--- server/config/socket.conf.js
+++ server/config/socket.conf.js
@@ -2,25 +2,41 @@ module.exports.listen = (server) => {
     const socketio = require('socket.io');
     io = socketio(server);
     io.on('connect', (socket) => {
-        console.log('user connected')
+        console.log('user connected');
         socket.on('disconnect', () => {
-            console.log('user disconnected')
+            console.log('user disconnected');
         })
-        socket.on('new-message', (msg) => {
-            io.emit('new-message', { msg })
+        socket.on('gameJoin', (gameId) => {
+            socket.gameRoom = gameId;
+            socket.join(gameId, ()=>{
+                socket.to(socket.gameRoom).emit('getState');
+            });
+        })
+        socket.on('gameLeave', () => {
+            socket.gameRoom = null;
+            socket.rooms = {};
+        })
+        socket.on('newMessage', (msg) => {
+            io.to(socket.gameRoom).emit('newMessage', msg);
         })
         socket.on('draw', (draw) => {
-            io.emit('draw', draw)
+            io.to(socket.gameRoom).emit('draw', draw);
         })
         socket.on('newTopic', (topic) => {
-            io.emit('newTopic', topic)
+            io.to(socket.gameRoom).emit('newTopic', topic);
         })
         socket.on('newRound', (roundInfo) => {
-            io.emit('newRound', roundInfo)
+            io.to(socket.gameRoom).emit('newRound', roundInfo);
         })
         socket.on('win', (winInfo) => {
-            io.emit('win', winInfo)
+            io.to(socket.gameRoom).emit('win', winInfo);
+        })
+        socket.on('gameEnd', (gameEnd) => {
+            io.to(socket.gameRoom).emit('gameEnd', gameEnd);
+        })
+        socket.on('setState', (state)=>{
+            io.to(socket.gameRoom).emit('setState', state);
         })
     })
-    return io
+    return io;
 }
