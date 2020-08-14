c97dbd5134f9dc8ab2bada392a82c4a307515855
unknown <kylet@LWNB02.Lockwood.local>
2020-08-14 13:38:02 -0500

    addedTopicFunctions

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { SocketService } from '../services/socket.service';
 import { FormControl } from '@angular/forms';
+import { GameService } from '../services/game.service';
 
 @Component({
   selector: 'app-game',
@@ -12,13 +13,19 @@ export class GameComponent implements OnInit, OnDestroy{
 
   displayName: string = '';
   savedName: boolean = false;
+  currentGame: string = this.actvr.snapshot.params.gameId;
 
-  constructor(private socket: SocketService, private actvr: ActivatedRoute) { }
+  constructor(private socket: SocketService, private actvr: ActivatedRoute, private gameService: GameService) { }
 
   saveDisplayName(){
     this.displayName = this.displayName;
     this.savedName = true;
   }
+
+  newTopic(currentGame){
+    this.gameService.newTopic(currentGame);
+  }
+
 
   ngOnInit(): void {
     this.socket.joinGame(this.actvr.snapshot.params.gameId);
