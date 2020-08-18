dcc66dbf8190a91b3df5111d00cf0017b5a7121a
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2020-08-18 09:23:29 -0500

    Function to delete old games ouf of DB

--- src/app/services/game.service.ts
+++ src/app/services/game.service.ts
@@ -1,9 +1,15 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Query } from '@angular/core';
 import { Router } from '@angular/router';
 import { Player } from '../interfaces/player.interface';
-import { AngularFirestore } from "@angular/fire/firestore"
+import { AngularFirestore, fromCollectionRef } from "@angular/fire/firestore"
 import { SocketService } from './socket.service';
 import topics from '../shared/topics.arrays';
+import { Time } from '@angular/common';
+import { time } from 'console';
+import { Timestamp } from 'rxjs/internal/operators/timestamp';
+import { firestore } from 'firebase';
+import { element } from 'protractor';
+import { query } from '@angular/animations';
 
 @Injectable({
   providedIn: 'root'
@@ -20,16 +26,23 @@ export class GameService {
   // Create game
   createGame(gameConfig, host: Player){
     this.gameId = Math.random().toString(36).substring(2, 4) + Math.random().toString(36).substring(2, 8);
+  // The function below is for setting an experation time of 2hrs ofter new game is created
+    let add_hours = function (dt, minutes) {
+      return new Date(dt.getTime() + minutes*3600000);
+  }
+    let validGameUntilTime = (add_hours(new Date(), 2).toString());
+  // End
+    let createdTime = new Date();
 
       this.socketService.createGame(this.gameId);
       console.log(this.gameId);
 
       this.FS.collection('pictionary').add({
-        createdTime: new Date(),
+        createdTime,
         currentArtist: host,
         currentTopic: this.randomTopic,
         gameId: this.gameId,
-        validGameUntilTime: new Date(),
+        validGameUntilTime,
         gameConfig,
         users: [host]
       }).then(res => this.router.navigate([`/game/${this.gameId}`]) )
@@ -74,5 +87,15 @@ export class GameService {
   // Game end function(s)
     // declare game winner
     // navigate to home - dialog box?
- 
-}
+
+  //  Removes old games from database(firestor)
+  
+
+
+  removeOldGames(gameId, createdTime){
+    if(this.FS.collection('pictiionary', ref => ref
+      .where('validGameUntilTime', "<", createdTime )).valueChanges()
+    )
+   );   
+    
+  };
