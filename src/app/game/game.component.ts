8ae2618956de26cb904cf783705b0ccd24168e6d
matticec <MY_NAME@example.com>
2020-08-20 11:57:23 -0500

    updated close room button to trigger function

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -39,6 +39,9 @@ export class GameComponent implements OnInit, OnDestroy{
   leaveGame(){
     this.gameService.leaveGame(this.displayName, this.currentGame)
   }
+  closeRoom(){
+    this.gameService.closeRoom(null)
+  }
   newTopic(){
     this.gameService.newTopic();
     if(this.timer == 60){
